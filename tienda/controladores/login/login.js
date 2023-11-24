f7cb8f1f83c38fd5b380c26676f3805b6119bde5
santii_ander <santiagoandermatten1@gmail.com>
2023-11-24 16:39:40 -0300

    xd

--- tienda/controladores/login/login.js
+++ tienda/controladores/login/login.js
@@ -4,91 +4,41 @@ import { usuariosServices } from "../../../servicios/usuarios-servicios.js";
 /**1- Se debe asignar a la siguiente constante todo el código correspondiente al componente de login (/asset/modulos/login.html)  */
 const htmlLogin=
 `
-<div class="hold-transition login-page" id="frmLogin">
-    <div class="login-box">
-      <div class="login-logo">
-        <img src="/img/plantilla/logo_light.png" style="opacity: .8">
-      </div>
-      <!-- /.login-logo -->
-      <div class="card">
-        <div class="card-body login-card-body">
-          <p class="login-box-msg">Iniciar sesión</p>
-  
-          <form  class="needs-validation" novalidate>
-  
-            <div class="input-group mb-3">
-              
-              <input 
-              type="email" 
-              class="form-control" 
-              id="loginEmail"
-              placeholder="Email"
-              name="loginEmail"
-              required>
-  
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-envelope"></span>
-                </div>
-              </div>
-  
-              <div class="valid-feedback">Válido.</div>
-              <div class="invalid-feedback">Please fill out this field.</div>
-  
-            </div>
-  
-            <div class="input-group mb-3">
-              
-              <input 
-              type="password" 
-              class="form-control" 
-              id="loginPassword"
-              placeholder="Password"
-              name="loginPassword"
-              required>
-  
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-lock"></span>
-                </div>
-              </div>
-  
-              <div class="valid-feedback">Válido.</div>
-              <div class="invalid-feedback">Please fill out this field.</div>
-  
-            </div>
-  
-           
-            <div class="row">
-              <div class="col-8">
-                <div class="icheck-primary">
-                  <input type="checkbox" id="remember" onchange="rememberMe(event)">
-                  <label for="remember">
-                    Remember Me
-                  </label>
-                </div>
-              </div>
-  
-              <!-- /.col -->
-              <div class="col-4">
-                <button type="button"  id="iniciar-sesion" class="btn bg-black btn-block">Sign In</button>
-              </div>
-  
-              <!-- /.col -->
-            </div>
-          </form>
-  
-          <!-- /.social-auth-links -->
-        </div>
-        <!-- /.login-card-body -->
-      </div>
-    </div>
-    <!-- /.login-box -->
-  
-  <!-- jQuery -->
-  </div>
-  
+<div class="contenedorLogin">
+    <div class="cajaLogin">
+        <p >Iniciar sesión</p>
 
+        <form  class="formLogin" >
+
+            <div class="input-group">
+                
+                <input type="email" class="form-control" id="loginEmail" placeholder="Email" name="loginEmail" autocomplete required>
+                
+            </div>
+
+            <div class="input-group">
+                
+                <input type="password" class="form-control" id="loginPassword" placeholder="Password" name="loginPassword" autocomplete required>
+            
+            </div>
+
+            <div class="input-group">
+                
+                <input type="password" class="form-control" id="reLoginPassword" placeholder="Repetir Password" name="reLoginPassword"  required>
+            
+            </div>
+                        
+            <div class="row">
+                                
+                <div class="col-4">
+                <button type="submit"  id="iniciar-sesion" class="btnAmarillo">Login</button>
+                </div>
+                    
+            </div>
+        </form>
+            
+    </div>
+</div>
 `;
 /*2-Se deben definir 4 variables globales al módulo, una para el formulario html, y otras tres para los inputs de email, contraseña y 
 *   repetir contraseña
@@ -102,9 +52,10 @@ var inputRepetirPass;
 
 export async function login(){
     /** 3- Esta función se encarga de llamar a la función crearFormulario y de enlazar el evento submit del formulario de login
-     * 
-    */
-    
+     *
+    **/
+   crearFormulario();
+   
 }  
 
 export async function register(){
