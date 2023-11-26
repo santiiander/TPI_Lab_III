dcac08580891a89f1c439bf242deafa38cff2204
LeonFGC <141806473+LeonFGC@users.noreply.github.com>
2023-11-26 19:15:43 -0300

    si

--- tienda/controladores/listarProductos/listarProductos.js
+++ tienda/controladores/listarProductos/listarProductos.js
@@ -1,54 +1,93 @@
 import { categoriasServices } from "../../../servicios/categorias-servicios.js";
 import { productosServices } from "../../../servicios/productos-servicios.js";
 
-function htmlCategoria(id, categoria) {
-    return `
-        <!-- Código HTML correspondiente al componente listarProducto.html -->
-        <div class="categoria" data-idCategoria="${id}">
-            <h2>${categoria}</h2>
-            <!-- Otro contenido de la categoría si es necesario -->
-        </div>
-    `;
+function htmlCategoria(id, categoria){
+    /*ESTA FUNCION RECIBE DOS PARAMETROS ID Y CATEGORIA*/
+    /*EN ESTA SE GENERA UNA CADENA DE CARACTERES CON EL CODIGO HTML CORRESPONDIENTE A LA CATEGORIA (ESTA EN ASSETS/MODULOS/listarProducto.html)*/
+    /*SE DEBERÁ CONCATENAR PARA INCORPORAR EL id DE LA CATEGORIA AL ATRIBUTO data-idCategoria  */
+    /*Y ADEMAS REEMPLAZAR EL TEXTO Nombre de Categoría POR EL VALOR QUE LLEGA AL PARAMETRO CATEGORIA DE LA FUNCION*/
+    /*POR ULTIMO, LA FUNCION DEVOLVERA LA CADENA RESULTANTE*/   
+    let cad = 
+                `<div class="categorias" data-idCategoria="${id}">
+                    <h1 class="categoria">${categoria}</h1>
+                    <div class="productos">
+
+                        <!-- Acá listan los productos-->
+                        <p class="item-producto">Sin productos.</p>
+                    </div>
+                </div>            
+                `;
+    return cad; 
 }
 
-function htmlItemProducto(id, imagen, nombre, precio) {
-    return `
-        <!-- Código HTML correspondiente al componente itemProducto.html -->
-        <div class="producto">
-            <h3>${id}</h3> 
-            <img src="${imagen}" alt="${nombre}">
-            <h3>${nombre}</h3>
-            <p>Precio: $${precio}</p>
-            <!-- Otro contenido del producto si es necesario -->
-        </div>
-    `;
+function htmlItemProducto(id, imagen, nombre, precio){
+    /**1- ESTA FUNCION RECIBE COMO PARAMETRO los siguiente datos id, imagen, nombre y precio del producto */
+    /**2- A ESTOS PARAMETROS LOS CONCATENA DENTRO DEL CODIGO CORRESPONDIENTE AL COMPONENTE itemProducto ( ASSETS/MODULOS/itemProducto.html)*/
+    /**3- POR ULTIMO DEVUELVE LA CADENA RESULTANTE. */
+    /**4- SE RECUERDA QUE PARA PODER HACER LA INTERPOLACION DE CADENAS ${NOMBRE_VARIABLE} EL TEXTO DEBE ESTAR ENTRE LAS COMILLAS ` `. 
+     *  
+     *  ejemplo
+     *   let titulo = 'Señora';  
+     *   let cadena = `Hola, ${titulo} Claudia  en que podemos ayudarla`;
+     *   
+    */
+    let cad = 
+                `<div class="item-producto">
+
+                <img src="${imagen}" >
+                <p class="producto_nombre" name="motorola">${nombre}</p>
+                <p class="producto_precio">${precio}</p>
+            
+                <a href="?idProducto=${id}#vistaProducto" type="button" class="producto_enlace" >Ver producto</a>
+            
+            </div>`;
+    return cad; 
+    
+
+
 }
 
-//La linea 17 no va, la agregué para usar ID
+async function asignarProducto(id){
+    /*1- ESTA FUNCION DEBERA CONSULTAR EN EL API-REST TODOS LOS PRODUCTOS PERTENECIENTES A LA CATEGORIA CON CODIGO ID  */
+    /*2- HACER UN BUCLE CON EL RESULTADO DE LA CONSULTA Y RECORRELO PRODUCTO POR PRODUCTO*/
+    /*3- EN EL INTERIOR DEL BUCLE DEBERA LLAMAR A LA FUNCION htmlItemProducto y acumular su resultado en una cadena de caracteres */
+    /*4- LUEGO DEL BUCLE Y CON LA CADENA RESULTANTE SE DEBE CAPTURAR EL ELEMENTO DEL DOM PARA ASIGNAR ESTOS PRODUCTOS DENTRO DE LA CATEGORIA CORRESPONDIENTE */
+    /*5- PARA ELLO PODEMOS HACER USO DE UN SELECTOR CSS QUE SELECCIONE EL ATRIBUTO data-idCategoria=X, Ó LA CLASE .productos  .SIENDO X EL VALOR LA CATEGORIA EN CUESTION.*/ 
+     
+    let d = document;
+    let cad = "";
+    let resProd = await productosServices.listarPorCategoria(id);
 
-async function asignarProducto(id) {
-    const productos = await productosServices.listarPorCategoria(id);
-    let htmlProductos = "";
+    resProd.forEach(producto => {
+        cad += htmlItemProducto(producto.id,producto.foto,producto.nombre,producto.precio);
+    });
+        
+    let itemProducto = d.querySelector("[data-idCategoria='"+ id + "'] .productos");
+    itemProducto.innerHTML = cad; 
+} 
 
-    for (const producto of productos) {
-        htmlProductos += htmlItemProducto(producto.id, producto.imagen, producto.nombre, producto.precio);
-    }
 
-    const categoriaElement = document.querySelector(`[data-idCategoria="${id}"]`);
-    categoriaElement.innerHTML = htmlProductos;
-}
+export async function listarProductos(){
+    /************************** .
+     /* 1- ESTA FUNCION DEBERA SELECCIONAR DESDE DEL DOM  LA CLASE .seccionProductos. */
+     /* 2- DEBERÁ CONSULTAR LA API-REST PARA TRAER LAS CATEGORIAS Y  CONSTRUIR UN BUCLE PARA RECORRERLAS UNA A UNA. */
+     /* 3- EN EL INTERIOR DE ESTE BUCLE LLAMARA A LA FUNCION htmlCategoria PARA ASIGNAR EL NOMBRE DE LA CATEGORIA Y SU ID*/
+     /* 4- SE DEBERA ASIGNAR EL RESULTADO DE FUNCION ANTERIOR AL ELEMENTO DEL DOM .seccionProductos */
+     /* 5- LUEGO DEBERÁ LLAMAR UNA FUNCION, asignarProducto, QUE RECIBA COMO PARAMETRO EL ID DE LA CATEGORIA  */
+     /* 6- FIN DEL BUCLE Y FIN DE LA FUNCION */   
 
-export async function listarProductos() {
-    const seccionProductos = document.querySelector(".seccionProductos");
-    seccionProductos.innerHTML = "";
+     let d = document;
+     let resCat;
 
-    const categorias = await categoriasServices.listar();
+     let listaProductos = d.querySelector(".seccionProductos");
 
-    for (const categoria of categorias) {
-        const htmlCategoriaElement = htmlCategoria(categoria.id, categoria.nombre);
-        seccionProductos.innerHTML += htmlCategoriaElement;
+     listaProductos.innerHTML = "";
+     resCat =  await categoriasServices.listar();
 
-        // Asignar productos a la categoría
-        asignarProducto(categoria.id);
-    }
-}
+     resCat.forEach(element => {
+        listaProductos.innerHTML += htmlCategoria(element.id, element.descripcion);
+        asignarProducto(element.id);
+     })
+     
+}  
+
--- tienda/controladores/listarProductos/vistaProducto.js
+++ tienda/controladores/listarProductos/vistaProducto.js
@@ -1,67 +1,114 @@
+/**ESTE COMPONENTE SE ENCARGA DE MOSTRAR EL DETALLE DE UN PRODUCTO */
 import { productosServices } from "../../../servicios/productos-servicios.js";
 import { ventasServices } from "../../../servicios/ventas-servicios.js";
 import { getUsuarioAutenticado } from "../login/login.js";
 
-export async function vistaProducto() {
-    const carrusel = document.querySelector(".carrusel");
-    const seccionProducto = document.querySelector(".seccionProducto");
-    const seccionLogin = document.querySelector(".seccionLogin");
-    carrusel.innerHTML = "";
-    seccionProducto.innerHTML = "";
-    seccionLogin.innerHTML = "";
+export async function vistaProducto(){
+    /**1-En esta función se deben capturar los elementos html: .carrusel, .seccionProducto, .seccionLogin. Para luego 
+     * blanquear su contenido. 
+     * 2-Se deberá capturar el elemento .vistaProducto.
+     * 3-Se deberá llamar a la función leerParametro para recuperar de la url el idProducto. 
+     * 4-Luego se deberán leer los datos del producto indentificado con el idProducto recuperado.
+     * 5-Llamar a la función htmlVistaProducto.
+     * 6-El resultado de la función deberá asignarse al elemento .vistaProducto capturado previamente.
+     * 7-Se deberá capturar el elemento html correspondiente al anchor btnComprar y enlazar el evento click a la función registrarCompra.  
+    */
+   let d = document; 
+   let res; 
+   let carrusel = d.querySelector(".carrusel");
+   let seccionProductos = d.querySelector(".seccionProductos");
+   let vistaProducto = d.querySelector(".vistaProducto");
+   carrusel.innerHTML = "";
+   let seccionLogin = d.querySelector(".seccionLogin");
+   seccionLogin.innerHTML = "";
+   seccionProductos.innerHTML = "";
+   let idProducto = leerParametro();
 
-    const vistaProductoElement = document.querySelector(".vistaProducto");
-    const idProducto = leerParametro();
-    
-    if (!idProducto) {
-        // Si no se proporciona un idProducto, podrías mostrar un mensaje de error o redirigir a otra página.
-        console.error("Error: No se proporcionó un idProducto válido.");
-        return;
-    }
+   res = await productosServices.listar(idProducto);
 
-    const producto = await productosServices.obtenerPorId(idProducto);
-    const htmlProducto = htmlVistaProducto(producto.id, producto.nombre, producto.descripcion, producto.precio, producto.imagen);
-    vistaProductoElement.innerHTML = htmlProducto;
+   vistaProducto.innerHTML = htmlVistaProducto(res.id, res.nombre, res.descripcion, res.precio, res.foto);
 
-    const btnComprar = document.getElementById("btnComprar");
-    btnComprar.addEventListener("click", registrarCompra);
+   let btnComprar = d.getElementById("btnComprar");
+
+   btnComprar.addEventListener("click", registrarCompra);
 }
 
 function htmlVistaProducto(id, nombre, descripcion, precio, imagen) {
-    return `
-        <!-- Código HTML correspondiente al componente vistaProducto -->
-        <div class="producto">
-            <img src="${imagen}" alt="${nombre}">
-            <h2>${nombre}</h2>
-            <p>${descripcion}</p>
-            <p>Precio: $${precio}</p>
-            <a href="#" id="btnComprar" data-idproducto="${id}">Comprar</a>
+    /**1- ESTA FUNCION RECIBE COMO PARAMETRO los siguiente datos id, nombre, descripcion, precio e imagen del producto */
+    /**2- A ESTOS PARAMETROS LOS CONCATENA DENTRO DEL CODIGO CORRESPONDIENTE AL COMPONENTE vistaProducto ( ASSETS/MODULOS/vistaProducto.html)*/
+    /**3- POR ULTIMO DEVUELVE LA CADENA RESULTANTE. */
+    /**4- SE RECUERDA QUE PARA PODER HACER LA INTERPOLACION DE CADENAS ${NOMBRE_VARIABLE} EL TEXTO DEBE ESTAR ENTRE LAS COMILLAS ` `. 
+     *  
+     *  ejemplo
+     *   let titulo = 'Señora';  
+     *   let cadena = `Hola, ${titulo} Claudia  en que podemos ayudarla`;
+     *   
+    */
+
+    let cad =
+            `<div class="imagen">
+            <img src="${imagen}" alt="producto">
         </div>
-    `;
-}
-
-function leerParametro() {
-    const words = new URLSearchParams(window.location.search);
-    const idProducto = words.get("idProducto");
-    return idProducto ? idProducto.trim() : null;
-}
-
-function registrarCompra() {
-    const session = getUsuarioAutenticado();
+        <div class="texto">
+            <p id="nameProducto" data-idProducto=${id}>${nombre}</p>
+        
+            <p id="descripcionProducto">${descripcion}</p>
+        
+            <p id="precioProducto">${precio}</p>
+        
+            <div class="form-group">
+                <label for="cantidadProducto">Cantidad</label>
+                <input type="number" step="1" min ="1" value="1" id="cantidadProducto">
+            </div>
+        
+            <a id="btnComprar" >Comprar</a>
+        </div>`;
+    return cad; 
     
-    if (!session || !session.autenticado) {
-        alert("Debes iniciar sesión antes de realizar una compra.");
+}
+function leerParametro(){
+    // Captura el idProducto de la dirección URL enviada por la página que llama
+    const words = new URLSearchParams(window.location.search);
+    let cad = words.get("idProducto");
+    if (!cad) return null;
+    return cad.trim();
+}
+
+
+function registrarCompra(){
+    /**1-Esta función es la encargada de procesar el evento click del anchor btnComprar.
+     * 2-Luego deberá recuperar con la función getUsuarioAutenticado presente en el módulo login.js el objeto session
+     * 3-Si la propiedad autenticado del objeto session es falso, el usuario no ha iniciado sesión, y se deberá emitir 
+     *   una alerta que comunique al usuario que antes de realizar una compra debe haber iniciado sesión y salir de la 
+     * ejecución de la función.
+     * 4-Si la propiedad autenticado es true la ejecución continua.
+     * 5-En este punto se deben almacenar los datos necesario para registrar la venta.
+     * 5-Necesitamos idUsuario, emailUsuario, idProducto, nameProducto, cantidad y fecha.
+     * 6-Los dos primeros los extraemos del objeto session.
+     * 7-El resto de los datos los capturamos desde el objeto document utilizando los id: nameProducto, cantidadProducto. 
+     *   El idProducto lo recuperamos desde el atributo data-idproducto y a fecha la obtenemos desde la fecha del sistema con
+     *   el objeto Date() de javascript.
+     * 8-Una vez reunido todos los datos necesarios llamamos a la función ventasServices.crear pasando lo parámetros obtenidos. 
+     * 9-Luego de registrar la venta utilizando el objeto location.replace("tienda.html") renderizamos nuevamente la página 
+     *   dejando el sitio en el estado inicial.
+     * 10-Finalmente emitimos una alerta con la leyenda "Compra finalizada."
+     *     
+     */
+    let d = document;
+    let session = getUsuarioAutenticado();
+    
+    if(! session.autenticado) {
+        alert("Antes de comprar debe iniciar sesión");
         return;
     }
-
-    const idUsuario = session.idUsuario;
-    const emailUsuario = session.email;
-    const idProducto = this.getAttribute("data-idproducto");
-    const nombreProducto = document.getElementById("nombreProducto").innerText;
-    const cantidadProducto = document.getElementById("cantidadProducto").value;
-    const fecha = new Date().toISOString();
-
-    ventasServices.crear(idUsuario, emailUsuario, idProducto, nombreProducto, cantidadProducto, fecha);
+    
+    let cantidad = d.getElementById("cantidadProducto").value;
+    let idUsuario = session.idUsuario;
+    let emailUsuario = session.email;
+    let nameProducto = d.getElementById("nameProducto");
+    let idProducto = nameProducto.getAttribute("data-idproducto");
+    const fecha = new Date();
+    ventasServices.crear(idUsuario,emailUsuario,idProducto,nameProducto.textContent,cantidad,fecha,0);
     location.replace("tienda.html");
-    alert("Compra finalizada.");
+    alert("Compra realizada");
 }
