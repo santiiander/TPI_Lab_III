97d435f82b922aa11fcc60fc8598fcdceef2c87b
LeonFGC <141806473+LeonFGC@users.noreply.github.com>
2023-11-26 13:53:32 -0300

    login pero no loggea

--- tienda/controladores/login/login.js
+++ tienda/controladores/login/login.js
@@ -4,106 +4,188 @@ import { usuariosServices } from "../../../servicios/usuarios-servicios.js";
 /**1- Se debe asignar a la siguiente constante todo el código correspondiente al componente de login (/asset/modulos/login.html)  */
 const htmlLogin=
 `
-    ASIGNAR EL COMPONENTE CORRESPONDIENTE!!!!
+<div class="contenedorLogin">
+<div class="cajaLogin">
+    <p >Iniciar sesión</p>
+
+    <form  class="formLogin" >
+
+        <div class="input-group">
+            
+            <input type="email" class="form-control" id="loginEmail" placeholder="Email" name="loginEmail" autocomplete required>
+            
+        </div>
+
+        <div class="input-group">
+            
+            <input type="password" class="form-control" id="loginPassword" placeholder="Password" name="loginPassword" autocomplete required>
+        
+        </div>
+
+        <div class="input-group">
+            
+            <input type="password" class="form-control" id="reLoginPassword" placeholder="Repetir Password" name="reLoginPassword"  required>
+        
+        </div>
+                    
+        <div class="row">
+                            
+            <div class="col-4">
+            <button type="submit"  id="iniciar-sesion" class="btnAmarillo">Login</button>
+            </div>
+                
+        </div>
+    </form>
+        
+</div>
+</div>
 `;
 /*2-Se deben definir 4 variables globales al módulo, una para el formulario html, y otras tres para los inputs de email, contraseña y 
 *   repetir contraseña
 */
-var formulario;
-var inputEmail;
-var inputPassword;
-var inputRepetirPass;
+const formLogin = document.querySelector(".formLogin");
+const loginEmail = document.getElementById("loginEmail");
+const loginPassword = document.getElementById("loginPassword");
+const reLoginPassword = document.getElementById("reLoginPassword");
+
+export { htmlLogin, formLogin, loginEmail, loginPassword, reLoginPassword };
 
 
 
-export async function login(){
-    /** 3- Esta función se encarga de llamar a la función crearFormulario y de enlazar el evento submit del formulario de login
-     * 
-    */
-    
-}  
+export async function login() {
+    // 1. Llamamos a la función `crearFormulario()` para renderizar el formulario de login
+    crearFormulario();
+  
+    // 2. Enlazamos el evento `submit` del formulario con la función `enviarFormulario()`
+    formLogin.addEventListener("submit", enviarFormulario);
+  }
+  
 
-export async function register(){
-     /** 4- Esta función se encarga de llamar a la función crearFormulario y de enlazar el evento submit del formulario de registro.
-      *     Esta función es similar a la de login, pero en el llamado a la función crearFormulario lo hace pasando el valor true al 
-      *     al parámetro registro que espera función mencionada.
-      *     Por último enlaza el evento submit del formulario a la función registrarUsuario.
-     * 
-    */
-   
-}  
+  export async function register() {
+    // 1. Llamamos a la función `crearFormulario()` para renderizar el formulario de registro
+    crearFormulario(true);
+  
+    // 2. Enlazamos el evento `submit` del formulario con la función `registrarUsuario()`
+    formLogin.addEventListener("submit", registrarUsuario);
+}
+  
 
 
 
-function crearFormulario(registrar){
-    /**
-     * 1- Esta función deberá capturar el elemento cuya clase es .carrusel y le asignará en su interior un blanco para eliminar su contenido previo.
-     * 2- Deberá realizar lo mismo para la clase .seccionProductos y .vistaProducto.
-     * 3- Luego deberá capturar la .seccionLogin para asignarle el contenido html del componente login, el cual se encuentra previamente 
-     *    cargado en la constante htmlLogin.
-     * 4- Deberá capturar los id correspondientes a loginEmail, loginPassword y reLoginPassword para asignarlos a las variable definidas
-     *    inputEmail, inputPassword e inputRepetirPass.
-     * 5- En el caso que el parámetro registrar sea falso deberá eliminar el contenido del elemento id reLoginPassword.
-     * 6- Para el caso que el parámetro registrar sea verdadero deberá cambiar el valor de la propiedad css dysplay a block. De esta forma
-     *    el input reLoginPassword se mostrará en pantalla.
-     * 7- Por último se deberá capturar el formulario indentificado con la clase .formLogin y asignarlo a la variable global formulario.
-     */
-    
-} 
-
-async function  ingresar(e){
-    /**
-     * 1- Esta función tiene como objetivo controlar que el texto en inputEmail e inputPassword se corresponda con alguna cuenta almacenada
-     *    en el REST-API.
-     * 2- Para ello en primera instancia deberá cancelar el comportamiento por defecto del envento recibido . Para ello deberá
-     *    tomar el parámetro evento ( e ) y ejecutar el método preventDefault().
-     * 3- Luego se deberá llamar la función llamada usuarioExiste. La misma devuelve un valor falso si el usuario no existe y el id del 
-     *    usuario en el caso que la cuenta sea válida.
-     * 4- Através de una estructura de desición se deberá, en el caso de que el usuario sea válido :
-     *     a- Llamar a la función setUsuarioAutenticado (usuariosServices) pasandole como parámetro el valor true y el id del usuario. De esta forma dicha 
-     *        función guardará estos datos en el sessionStorage del navegado, para poder ser consultados en el momento de la compra.
-     *     b- Llamar a la función mostrarUsuario, pasandole como parámetro el texto del email de la cuenta.  
-     * 5- En el caso de que el usuario no sea válido se deberá mostrar una alerta con el texto 'Email o contraseña incorrecto, intenta nuevamente'.
-     */
-   
-
+function crearFormulario(registrar) {
+    // 1- Capturamos el elemento cuya clase es .carrusel y le asignamos en su interior un blanco para eliminar su contenido previo.
+    const carrusel = document.querySelector(".carrusel");
+    carrusel.innerHTML = "";
+  
+    // 2- Realizamos lo mismo para la clase .seccionProductos y .vistaProducto.
+    const seccionProductos = document.querySelector(".seccionProductos");
+    seccionProductos.innerHTML = "";
+    const vistaProducto = document.querySelector(".vistaProducto");
+    vistaProducto.innerHTML = "";
+  
+    // 3- Capturamos la .seccionLogin para asignarle el contenido html del componente login.
+    const seccionLogin = document.querySelector(".seccionLogin");
+    seccionLogin.innerHTML += htmlLogin;
+  
+    // 4- Capturamos los id correspondientes a loginEmail, loginPassword y reLoginPassword.
+    const inputEmail = document.getElementById("loginEmail");
+    const inputPassword = document.getElementById("loginPassword");
+    const inputRepetirPass = document.getElementById("reLoginPassword");
+  
+    // 5- En el caso que el parámetro registrar sea falso eliminamos el contenido del elemento id reLoginPassword.
+    if (!registrar) {
+      inputRepetirPass.value = "";
+    }
+  
+    // 6- Para el caso que el parámetro registrar sea verdadero cambiamos el valor de la propiedad css dysplay a block.
+    if (registrar) {
+      inputRepetirPass.style.display = "block";
+    }
+  
+    // 7- Capturamos el formulario indentificado con la clase .formLogin y asignamoslo a la variable global formulario.
+    formulario = document.querySelector(".formLogin");
 }
 
-async function  registrarUsuario(e){
-    /**
-     * 1- Esta función tiene como objetivo controlar que el texto en inputPassword sea exactamente igual al texto ingresado en
-     *    inputRepetirPass y luego registrar la cuenta en el REST-API.
-     * 2- Para ello en primera instancia deberá cancelar el comportamiento por defecto del envento recibido . Para ello deberá
-     *    tomar el parámetro evento ( e ) y ejecutar el método preventDefault().
-     * 3- Luego se comparará con una estructura de decisión si los textos ingresados en los controles mencionados son exactamente iguales.
-     * 4- En caso afirmativo utilizando usuariosServices mediante el método crear, dará de alta el nuevo usuario.
-     * 5- Deberá mostrar una alerta con la leyenda "Email registrado" y cambiará el valor del objeto window.location.href a "#login", para que
-     *    se muestre la pantalla de login. 
-     * 5- En caso negativo o falso mostrará una alerta indicando que las contraseñas ingresadas no son iguales.  
-     */
-   
-    
+async function ingresar(e) {
+    // 1- Cancelamos el comportamiento predeterminado del evento recibido
+    e.preventDefault();
+  
+    // 2- Obtenemos los valores de los campos del formulario
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+  
+    // 3- Llamamos a la función usuarioExiste()
+    const idUsuario = await usuariosServices.usuarioExiste(email, password);
+  
+    // 4- Analizamos el resultado de la función usuarioExiste()
+    if (idUsuario) {
+      // 4.a- Llamamos a la función setUsuarioAutenticado()
+      usuariosServices.setUsuarioAutenticado(true, idUsuario);
+  
+      // 4.b- Llamamos a la función mostrarUsuario()
+      mostrarUsuario(email);
+    } else {
+      // 5- Mostramos un mensaje de error
+      alert("Email o contraseña incorrecto, intenta nuevamente");
+    }
+  }
+
+async function registrarUsuario(e) {
+    // 1- Cancelamos el comportamiento predeterminado del evento recibido
+    e.preventDefault();
+  
+    // 2- Obtenemos los valores de los campos del formulario
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+    const rePassword = reLoginPassword.value;
+  
+    // 3- Comparamos los valores de los campos
+    if (password === rePassword) {
+      // 4- Registramos el usuario
+      const response = await usuariosServices.crear(email, password);
+  
+      // 5- Mostramos un mensaje de éxito
+      alert("Email registrado");
+      window.location.href = "#login";
+    } else {
+      // 6- Mostramos un mensaje de error
+      alert("Las contraseñas ingresadas no son iguales");
+    }
 }
+
 async function usuarioExiste() {
-    /**
-     * 1- El objetivo de esta función es consultar la lista de usuarios con la función usuariosServices.listar() y mediante
-     *    un bucle comparar el email y la contraseña ingresado por el usuario en inputEmail e inputPassword con los previamente
-     *    almacenados dentro del API-REST sobre el recuros usuarios.
-     * 2- Si el email y la contraseña son válidos devuelve el id de usuario.
-     * 3- Si el email y la contraseña no son válido devuelve falso.    
-     */
-    
+    // 1- Consultamos la lista de usuarios
+    const usuarios = await usuariosServices.listar();
+  
+    // 2- Iteramos sobre la lista de usuarios
+    for (const usuario of usuarios) {
+      // 3- Comparamos el email y la contraseña del usuario actual con los ingresados por el usuario
+      if (usuario.email === loginEmail.value && usuario.password === loginPassword.value) {
+        // 4- Si son iguales, devolvemos el ID del usuario
+        return usuario.id;
+      }
+    }
+  
+    // 5- Si no se encuentra ningún usuario con los datos ingresados, devolvemos falso
+    return false;
 }
 
-export function mostrarUsuario(email){
-    /**
-     * 1- Esta función deberá capturar del dom la clase .btnLogin y asignarle el texto existente en el parámetro email.
-     * 2- Deberá capturar del dom la clase .btnRegister y asignarle el texto "Logout" y a este elemento asignarle el valor
-     *    "#logout" sobre el atributo href.
-     **/
-    
-
-}
+export function mostrarUsuario(email) {
+    // 1- Capturamos la clase .btnLogin
+    const btnLogin = document.querySelector(".btnLogin");
+  
+    // 2- Asignamos el texto del email al botón
+    btnLogin.textContent = email;
+  
+    // 3- Capturamos la clase .btnRegister
+    const btnRegister = document.querySelector(".btnRegister");
+  
+    // 4- Asignamos el texto "Logout" al botón
+    btnRegister.textContent = "Logout";
+  
+    // 5- Asignamos el valor "#logout" al atributo href del botón
+    btnRegister.href = "#logout";
+  }
 
 function mostrarMensaje(msj) {
     /**
@@ -113,21 +195,28 @@ function mostrarMensaje(msj) {
 }
 
 export function setUsuarioAutenticado(booleano, idUsuario) {
-    /**
-     * 1- Esta función deberá registar en el sessionStorage tres valores: autenticado, idUsuario y email.
-     * 2- Los valores de los mismos serán tomados de los dos parámetros recibidos y el email será tomado desde la variable
-     *    inputEmail.
-     */
-    
+    // 1- Capturamos el email del formulario
+    const email = loginEmail.value;
+  
+    // 2- Guardamos los valores en el sessionStorage
+    sessionStorage.setItem("autenticado", booleano);
+    sessionStorage.setItem("idUsuario", idUsuario);
+    sessionStorage.setItem("email", email);
+  }
 
-
-}
-export function getUsuarioAutenticado() {
-    /**
-     * 1- Esta función debera leer los valores almacenados en el sessionStorage y construir un objeto con los valores
-     * autenticado, idUsuario y email.
-     * 2- Luego los devolverá como resultado.
-     */
-    
-       
+  export function getUsuarioAutenticado() {
+    // 1- Obtenemos los valores almacenados en el sessionStorage
+    const autenticado = sessionStorage.getItem("autenticado");
+    const idUsuario = sessionStorage.getItem("idUsuario");
+    const email = sessionStorage.getItem("email");
+  
+    // 2- Construimos un objeto con los valores obtenidos
+    const usuarioAutenticado = {
+      autenticado,
+      idUsuario,
+      email,
+    };
+  
+    // 3- Devolvemos el objeto
+    return usuarioAutenticado;
 }
